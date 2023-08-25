9c18322da88b2e355e52d359f1c18baafe498da1
JoaHoe <73616539+JoaHoe@users.noreply.github.com>
2023-08-25 10:10:59 +0200

    Add files via upload

---
+++ R/func/func_det_of_angle.R
@@ -0,0 +1,21 @@
+## name of script: func_det_of_angle.R
+# purpose: determination of angle (azimuth) at corner points
+# function used in: spObj_sequence_of_lines.R & support_sequence_of_lines.R
+# author: Joachim Höhle
+# GNU General Public License (GPL)
+
+det_of_angle <- function(corner_x,corner_y) { 
+  alph <- (atan2(-(corner_y-yc),(corner_x-xc)))*omega # -dy/dx because alpha 
+    #in math-coordinate-system
+  
+  if(alph < 0) {
+    alph3 <- alph + 360
+  } else {
+    alph3 <- alph
+  } #end if-else
+  
+  angle_center_corner <- alph3
+  return(angle_center_corner)
+} #end of function 'det_of_angle'
+
+#end of script "func_det_of_angle.R"
